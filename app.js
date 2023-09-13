01a568b86592e9ba434529507c5c5b286eaf5fe1
David Igbayilola <43897620+SoftwareSultanDavid@users.noreply.github.com>
2023-09-13 01:07:17 +0100

    Added changes and removed API in stage 2 task no query ONLY PATH parameter testing will be available on postman

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const port = 3000;
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+
+
 dotenv.config();
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); //FOR FORM DATA INPUTS
 
-// GETTING ALL Name IN THE DATABASE
+// GETTING ALL Name IN THE DATABASE OR READ OPERATION
 app.get('/api', async(req, res) => {
     try {
         const name = await Name.find({});
@@ -22,7 +24,10 @@ app.get('/api', async(req, res) => {
     }
 });
 
-// GETTING A SPECIFIC USER IN THE DATABASE
+
+// * NO QUERY PARAMETER ONLY PATH PARAMETER HERE (REMOVED QUERY PARAMETER ON THE ID)
+
+// GETTING A SPECIFIC USER IN THE DATABASE OR READ OPERATION
 app.get('/api/:user_id', async(req, res) => {
     try {
         // GETTING ID FROM THE PARAMETERS
@@ -35,41 +40,25 @@ app.get('/api/:user_id', async(req, res) => {
     }
 });
 
-// Dynamic searching for username
-app.get('/api/name/:user_name', async(req, res) => {
-    try {
-        // GETTING ID FROM THE PARAMETERS
-        const {user_name} = req.params;
-        const name = await Name.findOne({"name": user_name});
-        res.status(200).json(name);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message});
-    }
-});
 
-
-// UPDATEING THE Name IN THE DATABASE
+// UPDATE THE Name IN THE DATABASE OR UPDATE OPERATION
 app.put('/api/:user_id', async(req, res) => {
     try {
         const {user_id} = req.params;
         const name = await Name.findByIdAndUpdate(user_id, req.body);
-
         // Name Update Validation
         if(!name){
             return res.status(404).json({message: `We cannot find name with the ID ${user_id}`});
         }
         const updatedName = await Name.findById(user_id);
         res.status(200).json(updatedName);
-
-
     } catch (error) {
         res.json(500).json({message: error.message});
     }
 });
 
 
-// DELETING A NAME FROM THE DATABASE
+// DELETING A NAME FROM THE DATABASE OR DELETE OPERATION
 app.delete('/api/:user_id', async(req, res) => {
     try {
         // GETTING ID FROM THE PARAMETERS
@@ -80,7 +69,7 @@ app.delete('/api/:user_id', async(req, res) => {
         }
 
         res.status(200).json({
-            message: `Succesfully deleted the name with the ID ${user_id}`,
+            message: `successfully deleted the name with the ID ${user_id}`,
             name
         });
     } catch (error) {
@@ -93,7 +82,10 @@ app.delete('/api/:user_id', async(req, res) => {
 app.post('/api', async(req, res) => {
     try {
         const name = await Name.create(req.body)
-        res.status(200).json(name);
+        res.status(200).json({
+            message: "User created successfully",
+            name
+        });
     } catch (error) {
         console.log(error.message);
         res.status(500).json({message: error.message});
